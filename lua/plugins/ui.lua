27aaa3a94dcd579df19fbc7c0f03c856bbe9dbf6
Cristian Morales <crisdavidmm95.dev@gmail.com>
2026-02-21 23:24:56 -0500

    feat: switch to Catppuccin Mocha, add nvim-web-devicons, enable all integrations

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -5,15 +5,53 @@
 -- ============================================================
 return {
 
-  -- Colorscheme
+  -- Icons (used by neo-tree, bufferline, lualine, telescope, alpha)
   {
-    "folke/tokyonight.nvim",
+    "nvim-tree/nvim-web-devicons",
+    lazy = false,
+    opts = { default = true },
+  },
+
+  -- Colorscheme: Catppuccin Mocha
+  {
+    "catppuccin/nvim",
+    name     = "catppuccin",
     lazy     = false,
     priority = 1000,
-    opts     = { style = "night" },
-    config   = function(_, opts)
-      require("tokyonight").setup(opts)
-      vim.cmd("colorscheme tokyonight-night")
+    config   = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        integrations = {
+          treesitter    = true,
+          telescope     = { enabled = true },
+          which_key     = true,
+          bufferline    = true,
+          neotree       = true,
+          gitsigns      = true,
+          indent_blankline = { enabled = true },
+          trouble       = true,
+          alpha         = true,
+          nvim_surround = true,
+          mason         = true,
+          lsp_trouble   = true,
+          native_lsp    = {
+            enabled = true,
+            virtual_text = {
+              errors      = { "italic" },
+              hints       = { "italic" },
+              warnings    = { "italic" },
+              information = { "italic" },
+            },
+            underlines = {
+              errors      = { "underline" },
+              hints       = { "underline" },
+              warnings    = { "underline" },
+              information = { "underline" },
+            },
+          },
+        },
+      })
+      vim.cmd("colorscheme catppuccin-mocha")
     end,
   },
 
@@ -23,7 +61,7 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
       options = {
-        theme            = "tokyonight",
+        theme            = "catppuccin",
         globalstatus     = true,
         section_separators   = { left = "", right = "" },
         component_separators = { left = "", right = "" },
